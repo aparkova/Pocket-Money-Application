e5a2bbb28ed9217e7d5c2057f16b2a9c28a420d4
Adriana <adriana.parkova@rwth-aachen.de>
2022-12-16 12:18:25 +0100

    updated PmToken

--- contracts/PmToken.sol
+++ contracts/PmToken.sol
@@ -1,45 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import "./CodeStorage.sol";
-
 contract PmToken {
     struct Balance {
         uint256 balance;
         uint256[] allowedProducts;
     }
 
-    CodeStorage codeStorage;
-
-    uint256 public counter;
-    address public testAddress;
     string public name = "Pocket Money Token";
     string public symbol = "PMT";
     uint256 public totalSupply = 1000; //2**256 -1;
-    string public standard = "Ppocket Money Token v1.0";
+    string public standard = "Pocket Money Token v1.0";
     uint8 public constant decimals = 18;
+    uint256[] public ids = [67000000, 50000000, 6700000050000000];
 
     mapping(address => uint256) balances;
-    mapping(address => Balance[]) balanceToConditions1;
+    mapping(address => mapping(uint256 => Balance)) conditions;
 
     constructor() {
         balances[msg.sender] = totalSupply;
     }
 
-    function InheritContract(address _codestorage) public {
-        codeStorage = CodeStorage(_codestorage);
-    }
-
-    function compareArrays() public view returns (bool) {
-        return codeStorage.compareArrays();
-    }
-
-    function deleteAllowedProducts() public {
-        return codeStorage.deleteAllowedProducts();
-    }
-
-    function isEligibleSeller(address _address) public view returns (bool) {
-        return codeStorage.isEligibleSeller(_address);
+    function getBalance(address _address, uint256 _id)
+        public
+        view
+        returns (Balance memory)
+    {
+        return conditions[_address][_id];
     }
 
     // transfer pocket money - parent to kid
@@ -49,97 +36,78 @@ contract PmToken {
         balances[_to] += _amount;
     }
 
-    // buying products, resetting attached conditions (only for the kid)
-    function buyProducts(address _to, uint256 _amount) external payable {
-        require(compareArrays() == true, "Items are not allowed");
-        require(isEligibleSeller(_to) == true, "Seller is not eligible");
-        require(balances[msg.sender] >= _amount, "Not enough tokens");
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
-        deleteAllowedProducts();
+    function checkIfArraysAreTheSame(
+        uint256[] memory _storedArray,
+        uint256[] memory _newArray
+    ) public pure returns (bool) {
+        if (_storedArray.length != _newArray.length) {
+            return false;
+        } else {
+            for (uint256 i = 0; i < _storedArray.length; i++) {
+                if (_storedArray[i] != _newArray[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
-    // transfer pocket money - parent to kid with conditions
+    function checkIfAlreadyStored(
+        uint256[] memory _oldArray,
+        uint256[] memory _newArray
+    ) internal pure returns (bool) {
+        for (uint256 i = 0; i < _oldArray.length; i++) {
+            for (uint256 j = 0; j < _newArray.length; j++) {
+                if (_oldArray[i] == _newArray[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //function transfer with conditions
     function transferWithConditions(
         address _to,
+        uint256 _id,
         uint256 _amount,
-        uint256[] memory _conditions
+        uint256[] memory _allowedProducts
     ) external payable {
         require(balances[msg.sender] >= _amount, "Not enough tokens");
         balances[msg.sender] -= _amount;
         balances[_to] += _amount;
-        Balance memory balancewithConditions = Balance(_amount, _conditions);
-        balanceToConditions1[_to].push(balancewithConditions);
+        conditions[_to][_id].balance += _amount;
+        uint256[] memory combinedArray = new uint256[](
+            conditions[_to][_id].allowedProducts.length +
+                _allowedProducts.length
+        );
+        //check if the new array is already stored in the old array
+        // if the old array contains all the elements of the new array, then we don't need to store the new array
+        if (
+            checkIfAlreadyStored(
+                conditions[_to][_id].allowedProducts,
+                _allowedProducts
+            )
+        ) {
+            return;
+        }
+        //if the old array doesn't contain all the elements of the new array, then we need to attach only the different value to the array
+        for (
+            uint256 i = 0;
+            i < conditions[_to][_id].allowedProducts.length;
+            i++
+        ) {
+            combinedArray[i] = conditions[_to][_id].allowedProducts[i];
+        }
+        for (uint256 i = 0; i < _allowedProducts.length; i++) {
+            combinedArray[
+                i + conditions[_to][_id].allowedProducts.length
+            ] = _allowedProducts[i];
+        }
+        conditions[_to][_id].allowedProducts = combinedArray;
     }
 
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
     }
-
-    //set balancetoConditions
-    // function setBalanceToConditions(
-    //     address _address,
-    //     uint256[] memory _testArray
-    // ) public {
-    //     balanceToConditions[_address][balances[_address]] = _testArray;
-    // }
-
-    function getBalanceWithConditions(address _address)
-        public
-        view
-        returns (Balance[] memory)
-    {
-        return balanceToConditions1[_address];
-    }
-
-    // iterate the array of structs in balancetoConditions1 mapping and return the balance attribute of each struct
-    function getBalanceToConditions(address _address)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256[] memory balanceArr = new uint256[](
-            balanceToConditions1[_address].length
-        );
-        for (uint256 i = 0; i < balanceToConditions1[_address].length; i++) {
-            balanceArr[i] = balanceToConditions1[_address][i].balance;
-        }
-        return balanceArr;
-    }
-
-    // iterate the array of structs in balancetoConditions1 mapping and return the allowedProducts attribute of each struct
-    function getConditions(address _address)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256 count = 0;
-
-        for (uint256 i = 0; i < balanceToConditions1[_address].length; i++) {
-            for (
-                uint256 j = 0;
-                j < balanceToConditions1[_address][i].allowedProducts.length;
-                j++
-            ) {
-                // allowedProductsArr[count] = balanceToConditions1[_address][i].allowedProducts[j];
-                count++;
-            }
-        }
-        uint256[] memory allowedProductsArr = new uint256[](count);
-        count = 0;
-        for (uint256 i = 0; i < balanceToConditions1[_address].length; i++) {
-            for (
-                uint256 j = 0;
-                j < balanceToConditions1[_address][i].allowedProducts.length;
-                j++
-            ) {
-                allowedProductsArr[count] = balanceToConditions1[_address][i]
-                    .allowedProducts[j];
-                count++;
-            }
-        }
-        return allowedProductsArr;
-    }
-
-    // getConditions from the array of structs in balancetoConditions1 mapping and store the attributes of each struct in a separate array
 }
