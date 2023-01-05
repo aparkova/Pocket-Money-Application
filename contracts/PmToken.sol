b12aef4e03343915d078e62dfb90800188977f13
Adriana <adriana.parkova@rwth-aachen.de>
2023-01-06 00:40:16 +0200

    conditionalTransfer function is fixed

--- contracts/PmToken.sol
+++ contracts/PmToken.sol
@@ -21,53 +21,56 @@ contract PmToken {
         balances[msg.sender] = totalSupply;
     }
 
-    function getBalance(address _address, uint256 _id)
-        public
-        view
-        returns (Balance memory)
-    {
-        return conditions[_address][_id];
-    }
-
-    // transfer pocket money - parent to kid
+    // ERC20 transfer function
     function transfer(address _to, uint256 _amount) external payable {
         require(balances[msg.sender] >= _amount, "Not enough tokens");
         balances[msg.sender] -= _amount;
         balances[_to] += _amount;
     }
 
-    function checkIfArraysAreTheSame(
-        uint256[] memory _storedArray,
-        uint256[] memory _newArray
-    ) public pure returns (bool) {
-        if (_storedArray.length != _newArray.length) {
-            return false;
-        } else {
-            for (uint256 i = 0; i < _storedArray.length; i++) {
-                if (_storedArray[i] != _newArray[i]) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-
     function checkIfAlreadyStored(
         uint256[] memory _oldArray,
         uint256[] memory _newArray
     ) internal pure returns (bool) {
-        for (uint256 i = 0; i < _oldArray.length; i++) {
-            for (uint256 j = 0; j < _newArray.length; j++) {
-                if (_oldArray[i] == _newArray[j]) {
-                    return true;
-                }
+        for (uint256 i = 0; i < _newArray.length; i++) {
+            if (!contains(_oldArray, _newArray[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function contains(uint256[] memory _array, uint256 _value)
+        internal
+        pure
+        returns (bool)
+    {
+        for (uint256 i = 0; i < _array.length; i++) {
+            if (_array[i] == _value) {
+                return true;
             }
         }
         return false;
     }
 
-    //function transfer with conditions
-    function transferWithConditions(
+    function concat(uint256[] memory _array1, uint256[] memory _array2)
+        internal
+        pure
+        returns (uint256[] memory)
+    {
+        uint256[] memory result = new uint256[](
+            _array1.length + _array2.length
+        );
+        for (uint256 i = 0; i < _array1.length; i++) {
+            result[i] = _array1[i];
+        }
+        for (uint256 i = 0; i < _array2.length; i++) {
+            result[_array1.length + i] = _array2[i];
+        }
+        return result;
+    }
+
+    function conditionalTransfer(
         address _to,
         uint256 _id,
         uint256 _amount,
@@ -78,81 +81,132 @@ contract PmToken {
         balances[_to] += _amount;
         conditions[_to][_id].balance += _amount;
 
-        //array1 and array2, and adds the elements in array2 that are not present in array1 to the end of array1. The difference array is used to store the elements that are present in array2 but not in array1
-        // if the new array doesn't have any values that are present in the old array, then attach the new array to the old one
-
-        if (
-            !checkIfAlreadyStored(
-                conditions[_to][_id].allowedProducts,
-                _allowedProducts
-            )
-        ) {
-            for (uint256 i = 0; i < _allowedProducts.length; i++) {
-                conditions[_to][_id].allowedProducts.push(_allowedProducts[i]);
-            }
-        }
-
-        uint256[] memory difference = new uint256[](
-            _allowedProducts.length -
-                conditions[_to][_id].allowedProducts.length
-        );
-        uint256 k = 0;
+        bool allElementsPresent = true;
         for (uint256 i = 0; i < _allowedProducts.length; i++) {
-            if (
-                !checkIfAlreadyStored(
-                    conditions[_to][_id].allowedProducts,
-                    _allowedProducts
-                )
+            bool elementPresent = false;
+            for (
+                uint256 j = 0;
+                j < conditions[_to][_id].allowedProducts.length;
+                j++
             ) {
-                difference[k] = _allowedProducts[i];
-                k++;
+                if (
+                    _allowedProducts[i] ==
+                    conditions[_to][_id].allowedProducts[j]
+                ) {
+                    elementPresent = true;
+                    break;
+                }
+            }
+            if (!elementPresent) {
+                allElementsPresent = false;
+                break;
             }
         }
-        for (uint256 i = 0; i < difference.length; i++) {
-            conditions[_to][_id].allowedProducts.push(difference[i]);
-        }
 
-        conditions[_to][_id].allowedProducts = _allowedProducts;
+        if (!allElementsPresent) {
+            // Concatenate the two arrays
+            uint256[] memory difference = new uint256[](
+                _allowedProducts.length
+            );
+            uint256 k = 0;
+            for (uint256 i = 0; i < _allowedProducts.length; i++) {
+                bool elementPresent = false;
+                for (
+                    uint256 j = 0;
+                    j < conditions[_to][_id].allowedProducts.length;
+                    j++
+                ) {
+                    if (
+                        _allowedProducts[i] ==
+                        conditions[_to][_id].allowedProducts[j]
+                    ) {
+                        elementPresent = true;
+                        break;
+                    }
+                }
+                if (!elementPresent) {
+                    difference[k] = _allowedProducts[i];
+                    k++;
+                }
+            }
+
+            uint256[] memory result = new uint256[](
+                conditions[_to][_id].allowedProducts.length + k
+            );
+            for (
+                uint256 i = 0;
+                i < conditions[_to][_id].allowedProducts.length;
+                i++
+            ) {
+                result[i] = conditions[_to][_id].allowedProducts[i];
+            }
+            for (uint256 i = 0; i < k; i++) {
+                result[
+                    i + conditions[_to][_id].allowedProducts.length
+                ] = difference[i];
+            }
+            conditions[_to][_id].allowedProducts = result;
+        }
     }
 
-    //check if conditions stored for a specific address are fulfilled, when the kid wants to spend the pocket money
-    function checkConditions(
-        address _from,
-        uint256 _id,
-        uint256 _amount,
-        uint256[] memory _products
-    ) public view returns (bool) {
-        if (conditions[_from][_id].balance >= _amount) {
-            if (
-                checkIfArraysAreTheSame(
-                    conditions[_from][_id].allowedProducts,
-                    _products
-                )
+    function buyProducts(
+        address _to,
+        uint256[] memory _ids,
+        uint256[] memory _amounts,
+        uint256[] memory _productCodes
+    ) public payable {
+        for (uint256 i = 0; i < _ids.length; i++) {
+            // Check if the product is in the allowedProducts array for the sender
+            bool productAllowed = false;
+            for (
+                uint256 j = 0;
+                j < conditions[msg.sender][_ids[i]].allowedProducts.length;
+                j++
             ) {
-                return true;
+                if (
+                    contains(
+                        _productCodes,
+                        conditions[msg.sender][_ids[i]].allowedProducts[j]
+                    )
+                ) {
+                    productAllowed = true;
+                    break;
+                }
+            }
+
+            if (productAllowed) {
+                // Check if the sender has enough balance in the conditions mapping to make the purchase
+                require(
+                    conditions[msg.sender][_ids[i]].balance >= _amounts[i],
+                    "Not enough balance"
+                );
+
+                // Make the transfer
+                conditions[_to][_ids[i]].balance += _amounts[i];
+                conditions[msg.sender][_ids[i]].balance -= _amounts[i];
+            } else {
+                revert(
+                    "One or more of the product codes are not allowed for purchase"
+                );
             }
         }
-        return false;
     }
 
-    //spend pocket money - kid to merchant
-    //function takes as parameters the seller's address, the id of the conditions, the amount of pocket money to spend, and the products bought
-    function spend(
-        address _from,
-        uint256 _id,
-        uint256 _amount,
-        uint256[] memory _products
-    ) external payable {
-        require(
-            checkConditions(_from, _id, _amount, _products),
-            "Conditions not fulfilled"
-        );
-        balances[_from] -= _amount;
-        balances[msg.sender] += _amount;
-        conditions[_from][_id].balance -= _amount;
+    // reset conditions mapping
+    function resetConditions(address _address, uint256 _id) external {
+        conditions[_address][_id].balance = 0;
+        conditions[_address][_id].allowedProducts = new uint256[](0);
     }
 
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
+    }
+
+    function getBalance(address _address, uint256 _id)
+        public
+        view
+        returns (Balance memory)
+    {
+        return conditions[_address][_id];
     }
 }
